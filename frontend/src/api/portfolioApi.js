ae54c26f42189d9135e8f09f751f23ebfa6049a3
xizershivas <xizershivas@gmail.com>
2026-04-25 08:01:18 +0800

    Update the UI animation

--- frontend/src/api/portfolioApi.js
+++ frontend/src/api/portfolioApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: 'http://localhost:5000/api' })
+const api = axios.create({ baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5139/api' })
 
 export const getProjects     = ()      => api.get('/projects')
 export const getFeatured     = ()      => api.get('/projects/featured')
